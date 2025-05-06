1bb571bba4b9d0e0fac9f78003c6b8c2c810ec0d
DC_Harisael <tim168950@gmail.com>
2025-05-06 15:43:52 -0400

    chore: change prerender config

--- svelte.config.js
+++ svelte.config.js
@@ -23,17 +23,7 @@ const config = {
       $i18n: './src/i18n',
     },
     prerender: {
-      entries: [
-        '/en',
-        '/ja',
-        '/zh',
-        '/zh-Hans',
-        '/zh-Hant',
-        '/example',
-        '/hello-QWER',
-        '/quick-start',
-        '/versions.json',
-      ],
+      entries: ['/en', '/zh', '/about', '/example', '/versions.json'],
     },
   },
 };
